772cd5db8ecbf1687b97c61971b6e983f1991d6d
renkin4 <yangah9991@gmail.com>
2017-11-04 11:30:58 +0800

    Initialize

---
+++ Source/GameJamLevelUpKL2017/GameJamLevelUpKL2017.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GameJamLevelUpKL2017.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameJamLevelUpKL2017, "GameJamLevelUpKL2017" );
---
+++ Source/GameJamLevelUpKL2017/GameJamLevelUpKL2017GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameJamLevelUpKL2017GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEJAMLEVELUPKL2017_API AGameJamLevelUpKL2017GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
